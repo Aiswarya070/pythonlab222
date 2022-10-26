a689de6a8ed436aca1cdb9567f4aae9a9669f9da
098aiswarya <105411948+098aiswarya@users.noreply.github.com>
2022-10-26 16:07:36 +0530

    Add files via upload
    area of triangle

---
+++ python area.py
@@ -0,0 +1,5 @@
+#to find the area and perimeter of a rectanglel
+l=int(input("enter the length:"))
+b=int(input("enter the breadth:"))      
+print("the area is", l*b)
+print("perimeter is", 2*(l+b))
