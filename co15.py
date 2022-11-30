82fe69bd796a273529013b3b3e88847a21cb8566
098aiswarya <105411948+098aiswarya@users.noreply.github.com>
2022-11-30 16:16:35 +0530

    Add files via upload

--- co15.py
+++ co15.py
@@ -1,3 +1,3 @@
-color_list_1=set["white","pink","red","blue"]
-color_list_2=set["blue","yellow","violet","green"]
+color_list_1=set(["white","pink"])
+color_list_2=set(["yellow","white"])
 print(color_list_1.difference(color_list_2))
