f8721da3c319a1b776d15790fae9e215d58a876b
Aiswarya070 <119944441+Aiswarya070@users.noreply.github.com>
2022-12-07 16:24:02 +0530

    Add files via upload

---
+++ CO1/co16.py
@@ -0,0 +1,5 @@
+str1=input("enter the 1st string:")
+str2=input("enter the 2nd string:")
+s1=str1[0]
+s2=str2[0]
+print("output:",s2+str1[1:],"and",s1+str2[1:])
---
+++ CO1/co6.py
@@ -0,0 +1,5 @@
+names=["aishu","aparna","anjali"]
+count=0
+for name in names:
+        count=count+name.count('a')
+print("count of occurences of a:",count)
